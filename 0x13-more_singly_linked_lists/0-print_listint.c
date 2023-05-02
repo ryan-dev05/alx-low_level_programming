b0aff49f9a42ba2624a08bfe2c749644ed43f285
ryan-dev05 <rgitau011@gmail.com>
2023-05-02 02:37:52 -0700

    print listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,17 +4,17 @@
  * print_listint - prints all elements of a function
  * @h: type listint_t to point
  *
- * Return: num of node
+ * Return:the number of node
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t k = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		printf("%d/n", h->n);
-		k++;
+		printf("%d\n", h->n);
 		h = h->next;
+		k++;
 	}
 
 	return (k);
