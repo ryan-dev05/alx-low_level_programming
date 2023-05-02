0b7b5a389829138104308ae6a98e06317fc1815a
ryan-dev05 <rgitau011@gmail.com>
2023-05-02 02:55:33 -0700

    listint len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns number of element in linked
+ * @h: linked list of listint_t
+ * Return: number of node
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
